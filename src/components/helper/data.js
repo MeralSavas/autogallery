810ca4b73c2efe8c16b09735449b2af08d864881
MeralSavas <meralsavas@gmail.com>
2022-10-13 13:22:23 +0100

    added new pictures

--- src/components/helper/data.js
+++ src/components/helper/data.js
@@ -35,10 +35,10 @@ const data = [
     },
   },
   {
-    photographer: "Dylan Chan",
+    photographer: "vintage 1932 ",
     src: {
       large:
-        "https://images.pexels.com/photos/4456088/pexels-photo-4456088.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        "https://cdn.pixabay.com/photo/2013/03/26/23/30/triumph-96981__340.jpg",
     },
   },
   {
@@ -56,199 +56,199 @@ const data = [
     },
   },
   {
-    photographer: "My Walking Diary",
+    photographer: "Fiat 1953",
     src: {
       large:
-        "https://images.pexels.com/photos/4115700/pexels-photo-4115700.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        "https://cdn.pixabay.com/photo/2013/06/05/04/30/fiat-116497__340.jpg",
     },
   },
   {
-    photographer: "lapography",
+    photographer: "Rolls Royce 1926",
     src: {
       large:
-        "https://images.pexels.com/photos/4426312/pexels-photo-4426312.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        "https://cdn.pixabay.com/photo/2013/06/05/20/41/rolls-royce-116948__340.jpg",
     },
   },
   {
-    photographer: "Reynaldo Yodia",
+    photographer: "Chrysler",
     src: {
       large:
-        "https://images.pexels.com/photos/13942344/pexels-photo-13942344.jpeg?auto=compress&cs=tinysrgb&w=600",
+        "https://cdn.pixabay.com/photo/2013/07/16/21/21/chrysler-163097__340.jpg",
     },
   },
   {
-    photographer: "Polina Chistyakova",
+    photographer: "v113 Mercedes",
     src: {
       large:
-        "https://images.pexels.com/photos/2815150/pexels-photo-2815150.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        "https://cdn.pixabay.com/photo/2017/06/05/15/02/w113-2374331__340.jpg",
     },
   },
   {
-    photographer: "Martin Péchy",
+    photographer: "Chrysler 1935",
     src: {
       large:
-        "https://images.pexels.com/photos/2078264/pexels-photo-2078264.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        "https://cdn.pixabay.com/photo/2013/09/05/12/30/chrysler-airstream-179089__340.jpg",
     },
   },
   {
-    photographer: "Javier Cruz",
+    photographer: "Jaguar",
     src: {
       large:
-        "https://images.pexels.com/photos/2917820/pexels-photo-2917820.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        "https://cdn.pixabay.com/photo/2016/10/04/21/22/car-1715496__340.jpg",
     },
   },
   {
-    photographer: "Maria Orlova",
+    photographer: "lancia spyder 1953",
     src: {
       large:
-        "https://images.pexels.com/photos/4913426/pexels-photo-4913426.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        "https://cdn.pixabay.com/photo/2013/06/05/04/31/lancia-spyder-116498__340.jpg",
     },
   },
   {
-    photographer: "Tom Fisk",
+    photographer: "auburn 1936",
     src: {
       large:
-        "https://images.pexels.com/photos/3413097/pexels-photo-3413097.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        "https://cdn.pixabay.com/photo/2016/12/27/06/13/auburn-1933317__340.jpg",
     },
   },
   {
-    photographer: "Souranshi Fashion",
+    photographer: "aston martin 1934",
     src: {
       large:
-        "https://images.pexels.com/photos/4497544/pexels-photo-4497544.jpeg?auto=compress&cs=tinysrgb&w=600",
+        "https://cdn.pixabay.com/photo/2013/06/05/02/00/aston-martin-116493__340.jpg",
     },
   },
   {
-    photographer: "Anastasiya Vragova",
+    photographer: "Hudson phaeton 1921",
     src: {
       large:
-        "https://images.pexels.com/photos/6711591/pexels-photo-6711591.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        "https://cdn.pixabay.com/photo/2013/01/30/02/01/hudson-phaeton-1921-76717__340.jpg",
     },
   },
   {
-    photographer: "ROMAN ODINTSOV",
+    photographer: "jaguar",
     src: {
       large:
-        "https://images.pexels.com/photos/4870581/pexels-photo-4870581.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        "https://cdn.pixabay.com/photo/2017/10/03/16/30/car-2813109__340.jpg",
     },
   },
   {
-    photographer: "Quang Nguyen Vinh",
+    photographer: "Duesenberg 1929",
     src: {
       large:
-        "https://images.pexels.com/photos/6875525/pexels-photo-6875525.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        "https://cdn.pixabay.com/photo/2013/06/07/17/51/duesenberg-122718__340.jpg",
     },
   },
   {
-    photographer: "Kris Møklebust",
+    photographer: "Chrysler 1934 ",
     src: {
       large:
-        "https://images.pexels.com/photos/12143040/pexels-photo-12143040.jpeg?auto=compress&cs=tinysrgb&h=650&w=940",
+        "https://cdn.pixabay.com/photo/2013/01/30/02/02/chrysler-1934-76720__340.jpg",
     },
   },
   {
-    photographer: "op23",
+    photographer: "Chevrolet Corvette",
     src: {
       large:
-        "https://images.pexels.com/photos/12125084/pexels-photo-12125084.jpeg?auto=compress&cs=tinysrgb&h=650&w=940",
+        "https://cdn.pixabay.com/photo/2021/09/16/11/12/chevrolet-corvette-stingray-6629842__340.jpg",
     },
   },
   {
-    photographer: "7inchs",
+    photographer: "Cadillac 1934",
     src: {
       large:
-        "https://images.pexels.com/photos/12092806/pexels-photo-12092806.jpeg?auto=compress&cs=tinysrgb&h=650&w=940",
+        "https://cdn.pixabay.com/photo/2019/05/23/02/48/cadillac-4223069_960_720.jpg",
     },
   },
   {
-    photographer: "Taylor Marx",
+    photographer: "Talbot logo 1937",
     src: {
       large:
-        "https://images.pexels.com/photos/12089012/pexels-photo-12089012.jpeg?auto=compress&cs=tinysrgb&h=650&w=940",
+        "https://cdn.pixabay.com/photo/2013/06/05/20/42/talbot-lago-116952__340.jpg",
     },
   },
   {
-    photographer: "Jair Hernandez",
+    photographer: "Lagonda 1935",
     src: {
       large:
-        "https://images.pexels.com/photos/12087895/pexels-photo-12087895.jpeg?auto=compress&cs=tinysrgb&h=650&w=940",
+        "https://cdn.pixabay.com/photo/2013/02/03/11/13/lagonda-77544__340.jpg",
     },
   },
   {
-    photographer: "Elena Rubtsova",
+    photographer: "Ferrari 500 1965",
     src: {
       large:
-        "https://images.pexels.com/photos/12082485/pexels-photo-12082485.jpeg?auto=compress&cs=tinysrgb&h=650&w=940",
+        "https://cdn.pixabay.com/photo/2013/02/03/11/13/ferrari-500-77551__340.jpg",
     },
   },
   {
-    photographer: "Elena Rubtsova",
+    photographer: "1912",
     src: {
       large:
-        "https://images.pexels.com/photos/12082493/pexels-photo-12082493.jpeg?auto=compress&cs=tinysrgb&h=650&w=940",
+        "https://cdn.pixabay.com/photo/2013/06/05/02/00/eijinsk-116492__340.jpg",
     },
   },
   {
-    photographer: "GEORGE KASCHEEV",
+    photographer: "Jaguar",
     src: {
       large:
-        "https://images.pexels.com/photos/12052041/pexels-photo-12052041.jpeg?auto=compress&cs=tinysrgb&h=650&w=940",
+        "https://cdn.pixabay.com/photo/2016/10/04/21/35/car-1715515__340.jpg",
     },
   },
   {
-    photographer: "Trinity LaKose",
+    photographer: "Bmw 1938",
     src: {
       large:
-        "https://images.pexels.com/photos/12050692/pexels-photo-12050692.jpeg?auto=compress&cs=tinysrgb&h=650&w=940",
+        "https://cdn.pixabay.com/photo/2013/02/03/11/13/bmw-77547__340.jpg",
     },
   },
   {
-    photographer: "Omar Zetina",
+    photographer: "Classic car",
     src: {
       large:
-        "https://images.pexels.com/photos/12046302/pexels-photo-12046302.jpeg?auto=compress&cs=tinysrgb&h=650&w=940",
+        "https://cdn.pixabay.com/photo/2014/12/27/14/36/car-show-581067__340.jpg",
     },
   },
   {
-    photographer: "Withsonya_",
+    photographer: "Chevrolet apache",
     src: {
       large:
-        "https://images.pexels.com/photos/12123082/pexels-photo-12123082.jpeg?auto=compress&cs=tinysrgb&h=650&w=940",
+        "https://cdn.pixabay.com/photo/2017/07/16/17/53/chevrolet-2510031__340.jpg",
     },
   },
   {
-    photographer: "Leah Kelley",
+    photographer: "Bmw 30 csl",
     src: {
       large:
-        "https://images.pexels.com/photos/12128380/pexels-photo-12128380.jpeg?auto=compress&cs=tinysrgb&h=650&w=940",
+        "https://cdn.pixabay.com/photo/2018/01/29/14/40/bmw-30-csl-3116267__340.jpg",
     },
   },
   {
-    photographer: "Leah Kelley",
+    photographer: "Car show",
     src: {
       large:
-        "https://images.pexels.com/photos/12128383/pexels-photo-12128383.jpeg?auto=compress&cs=tinysrgb&h=650&w=940",
+        "https://cdn.pixabay.com/photo/2014/12/27/14/36/car-show-581066__340.jpg",
     },
   },
   {
-    photographer: "Usman ARJ",
+    photographer: "Vintage car",
     src: {
       large:
-        "https://images.pexels.com/photos/12125024/pexels-photo-12125024.jpeg?auto=compress&cs=tinysrgb&h=650&w=940",
+        "https://cdn.pixabay.com/photo/2016/09/12/13/00/vintage-car-1664130__340.jpg",
     },
   },
   {
-    photographer: "Iury Bessa",
+    photographer: "Vintage car red",
     src: {
       large:
-        "https://images.pexels.com/photos/12118868/pexels-photo-12118868.jpeg?auto=compress&cs=tinysrgb&h=650&w=940",
+        "https://cdn.pixabay.com/photo/2015/07/20/00/55/vintage-car-852235__340.jpg",
     },
   },
   {
-    photographer: "op23",
+    photographer: "Chrysler",
     src: {
       large:
-        "https://images.pexels.com/photos/12118291/pexels-photo-12118291.jpeg?auto=compress&cs=tinysrgb&h=650&w=940",
+        "https://cdn.pixabay.com/photo/2015/11/07/16/30/car-1032068_960_720.jpg",
     },
   },
 ];
